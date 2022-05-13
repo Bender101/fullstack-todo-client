631959313cbee08d9e342783e50f5cdbc1855227
Bender101 <serkok227@gmail.com>
2022-05-13 22:08:09 +0300

    deploy

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUserAC, logoutUserAC } from "../../redux/thunk/loginAC";
 import "./Login.css";
